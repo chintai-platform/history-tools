99552e73611bf1b6b4b55917d0adc8378e5b4b17
Phillip Hamnett <phillip.hamnett@gmail.com>
2022-04-16 12:26:50 +0200

    Rewrite script so that you do not have to remember the password to open postgres

--- scripts/start_psql.sh
+++ scripts/start_psql.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo -n $RANDOM | md5sum | head -c 20 > ps_pass
+echo localhost:5432:postgres:postgres:$(cat ps_pass) > ~/.pgpass
+chmod 0600 ~/.pgpass
 
 docker run --rm -d -e POSTGRES_PASSWORD=`cat ps_pass` --rm --name postgres -p 0.0.0.0:5432:5432 postgres

3def82cedbe705a914be12c56045859e3e61c1ec
Michael Nash <michael.nash@chintai.io>
2022-07-08 16:02:13 +0200

    EX-1164 Attempt to resolve compilation problems

--- src/fill_pg_plugin.cpp
+++ src/fill_pg_plugin.cpp
@@ -738,7 +738,7 @@ struct fpg_session : connection_callbacks, std::enable_shared_from_this<fpg_sess
      std::cout << values.at(i) << std::endl;
    } 
 
-   if (values.at(1) == 0)
+   if (values.at(1) == "0")
    {
      write_table_row(block_num, values);
    }
